5fdf5263649203a1db7f7ad67819fc7ec6b880e5
Mathias Robert <mathias.robert@theoplayer.com>
2020-09-30 22:35:23 +0200

    Add basic resolvers

--- test-crud-server/src/resolvers.ts
+++ test-crud-server/src/resolvers.ts
@@ -1,5 +1,81 @@
+import { Op } from "sequelize";
+import { Product } from "./models/product";
+
+interface AddProductInput {
+  title: string;
+  description: string;
+  price: number;
+}
+
+interface UpdateProductInput {
+  id: string;
+  title: string;
+  description: string;
+  price: number;
+}
+
+interface DeleteProductInput {
+  id: string;
+}
+
+interface GetProductInput {
+  id: string;
+}
+
+const addProduct = async (args: AddProductInput) => {
+  const product = new Product({
+    title: args.title,
+    description: args.description,
+    price: args.price,
+  });
+  await product.save();
+
+  return product;
+};
+
+const updateProduct = async (args: UpdateProductInput) => {
+  const product = await Product.update(
+    {
+      title: args.title,
+      description: args.description,
+      price: args.price,
+    },
+    { where: { id: args.id } }
+  );
+
+  return product[0] === 1;
+};
+
+const deleteProduct = async (args: DeleteProductInput) => {
+  const isDeleted = await Product.destroy({ where: { id: args.id } });
+  return isDeleted === 1;
+};
+
+const getProducts = async () => {
+  return await Product.findAll();
+};
+
+const getProduct = async (args: GetProductInput) => {
+  const product = await Product.findOne({
+    where: {
+      id: {
+        [Op.eq]: args.id,
+      },
+    },
+  });
+
+  return product;
+};
+
 export const resolvers = {
-  Query: {},
-  Mutation: {},
+  Query: {
+    getProducts: () => getProducts(),
+    getProduct: async (_: any, args: GetProductInput) => getProduct(args),
+  },
+  Mutation: {
+    addProduct: (_: any, args: AddProductInput) => addProduct(args),
+    updateProduct: (_: any, args: UpdateProductInput) => updateProduct(args),
+    deleteProduct: (_: any, args: DeleteProductInput) => deleteProduct(args),
+  },
   Product: {},
 };
--- test-crud-server/src/schema.ts
+++ test-crud-server/src/schema.ts
@@ -2,18 +2,29 @@ import { gql } from "apollo-server-express";
 
 // GraphQL schema
 export const typeDefs = gql`
+  scalar DateTime
+
   type Product {
     id: ID!
     title: String!
     description: String!
     price: Float!
+    createdAt: DateTime!
   }
 
   type Query {
     getProducts: [Product]
+    getProduct(id: ID!): Product
   }
 
   type Mutation {
-    addProduct(title: String!, description: String!, price: Float): Product
+    addProduct(title: String!, description: String!, price: Float!): Product
+    updateProduct(
+      id: ID!
+      title: String!
+      description: String!
+      price: Float!
+    ): Boolean
+    deleteProduct(id: ID!): Boolean
   }
 `;
