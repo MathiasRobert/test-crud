710aeda9f93c504f4eaa746abbe44d52e131a089
Mathias Robert <mathias.robert@theoplayer.com>
2020-10-01 01:03:05 +0200

    heroku server

--- test-crud-server/src/index.ts
+++ test-crud-server/src/index.ts
@@ -15,6 +15,6 @@ server.applyMiddleware({ app });
 
 initDb();
 
-app.listen({ port: 4000 }, () =>
+app.listen({ port: process.env.PORT || 4000 }, () =>
   console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
